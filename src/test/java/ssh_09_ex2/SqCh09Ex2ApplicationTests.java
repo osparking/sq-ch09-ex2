816e43bdbf83c4ef254bf371122b60293899aef6
osparking <jbpark03@gmail.com>
2023-09-29 15:22:42 +0900

    initial of session-scoped bean, 3 dependencies

---
+++ src/test/java/ssh_09_ex2/SqCh09Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh_09_ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh09Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
